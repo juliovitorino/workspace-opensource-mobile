801860dfc4d13f41b28d40d506f23b1bb9c22e23
julio.vitorino <julio.vitorino@accountfy.com>
2025-06-14 18:05:33 -0300

    [140915] refactoring for new student page to bloc concepts

---
+++ treinadorpro/lib/features/newstudent/presentation/blocs/new_student_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:treinadorpro/core/states/handler_state.dart';
+
+class NewStudentCubit extends Cubit<HandlerState>{
+  NewStudentCubit() : super(HandlerState());
+
+  Future<void> register(String name, String email, String password, String apiKey) async {
+    emit(state.copyWith(isLoading: true, errorMessage: null));
+
+    // se tiver mais alguma regra de negócio de camada de visão pode colocar aqui
+    await Future.delayed(const Duration(seconds: 3)); // Simula API - chamada do meu backend
+
+    if (email.contains('@')) {
+      emit(state.copyWith(isLoading: false)); // Sucesso
+    } else {
+      emit(state.copyWith(isLoading: false, errorMessage: 'Email inválido'));
+    }
+  }
+}
