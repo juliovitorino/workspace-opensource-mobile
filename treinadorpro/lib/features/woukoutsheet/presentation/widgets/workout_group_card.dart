23e3851b301dc41e5fbb91a90a9d67090b4b4a89
julio.vitorino <julio.vitorino@accountfy.com>
2025-07-17 18:03:41 -0300

    [171802] Added icons group to control trash and expand icon behavior

--- treinadorpro/lib/features/woukoutsheet/presentation/widgets/workout_group_card.dart
+++ treinadorpro/lib/features/woukoutsheet/presentation/widgets/workout_group_card.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:treinadorpro/core/data/models/user_workout_plan_model.dart';
 
-class WorkoutGroupCard extends StatelessWidget {
+class WorkoutGroupCard extends StatefulWidget {
   final String groupName;
   final List<UserWorkoutPlanModel> exercises;
   final void Function(UserWorkoutPlanModel exercise)? onDelete;
@@ -15,6 +15,15 @@ class WorkoutGroupCard extends StatelessWidget {
   });
 
   @override
+  State<WorkoutGroupCard> createState() => _WorkoutGroupCardState();
+}
+
+class _WorkoutGroupCardState extends State<WorkoutGroupCard> {
+  bool _isShowDeleteIcon = false;
+  bool _isShowExercisesIcon = true;
+  bool _isShowDeleteGroupIcon = true;
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.all(12),
@@ -25,32 +34,64 @@ class WorkoutGroupCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              groupName,
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    widget.groupName,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+
+                // delete icon
+                if(_isShowDeleteGroupIcon)
+                  IconButton(
+                    onPressed: () => setState(() {
+                      _isShowDeleteIcon = !_isShowDeleteIcon;
+                    }),
+                    icon: Icon(Icons.delete),
+                  ),
+
+                // view icon
+                _isShowExercisesIcon
+                    ? IconButton(
+                        onPressed: () => setState(() {_isShowExercisesIcon = false; _isShowDeleteGroupIcon = false;}),
+                        icon: Icon(Icons.arrow_upward),
+                      )
+                    : IconButton(
+                        onPressed: () => setState(() {_isShowExercisesIcon = true;_isShowDeleteGroupIcon = true;}),
+                        icon: Icon(Icons.arrow_downward),
+                      ),
+              ],
             ),
             SizedBox(height: 12),
-            ...exercises.map(
-              (e) => ListTile(
-                leading: IconButton(
-                  onPressed: () => onDelete?.call(e),
-                  icon: const Icon(Icons.delete, color: Colors.red),
-                ),
-                title: Text(
-                  e.customExercise ?? e.exercise?.namePt ?? 'Exercício',
-                ),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '${e.qtySeries}x${e.qtyReps} • Executar em ${e.executionTime}m • descanso: ${e.restTime}m',
-                    ),
-                    SizedBox(height: 8,),
-                    Chip(label: Text(e.executionMethod.toString()))
-                  ],
+            if (_isShowExercisesIcon)
+              ...widget.exercises.map(
+                (e) => ListTile(
+                  leading: _isShowDeleteIcon
+                      ? IconButton(
+                          onPressed: () => widget.onDelete?.call(e),
+                          icon: const Icon(Icons.delete, color: Colors.red),
+                        )
+                      : null,
+                  title: Text(
+                    e.customExercise ?? e.exercise?.namePt ?? 'Exercício',
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '${e.qtySeries}x${e.qtyReps} • Executar em ${e.executionTime}m • descanso: ${e.restTime}m',
+                      ),
+                      SizedBox(height: 8),
+                      Chip(label: Text(e.executionMethod.toString())),
+                    ],
+                  ),
                 ),
               ),
-            ),
           ],
         ),
       ),
