9d7fc05bfd693bee1c30960e7697eddf040fa385
julio.vitorino <julio.vitorino@accountfy.com>
2025-11-08 13:52:26 -0300

    Ajustes de arquivos de configuração

--- treinadorpro/android/app/build.gradle.kts
+++ treinadorpro/android/app/build.gradle.kts
@@ -14,12 +14,13 @@ android {
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        // AGP 8+ recomenda Java 17
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
